901a87522bd8f3022d3e3c60ef03e3583ccec8be
Wesley Martins <wesley.mlopes@castgroup.com>
2024-04-12 08:32:36 -0300

    conexao mysql usando workflow code first do entity framework

--- Projeto_SalesWebMvc/Startup.cs
+++ Projeto_SalesWebMvc/Startup.cs
@@ -37,7 +37,7 @@ namespace Projeto_SalesWebMvc
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<Projeto_SalesWebMvcContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("Projeto_SalesWebMvcContext")));
+                    options.UseMySql(Configuration.GetConnectionString("Projeto_SalesWebMvcContext"), builder => builder.MigrationsAssembly("Projeto_SalesWebMvc")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

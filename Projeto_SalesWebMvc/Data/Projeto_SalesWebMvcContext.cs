aca5a87be8b002ca213d7663b3598ce29c2b0252
Wesley Martins <wesley.mlopes@castgroup.com>
2024-04-12 07:53:19 -0300

    crud scaffolding

---
+++ Projeto_SalesWebMvc/Data/Projeto_SalesWebMvcContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Projeto_SalesWebMvc.Models;
+
+namespace Projeto_SalesWebMvc.Data
+{
+    public class Projeto_SalesWebMvcContext : DbContext
+    {
+        public Projeto_SalesWebMvcContext (DbContextOptions<Projeto_SalesWebMvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Projeto_SalesWebMvc.Models.Department> Department { get; set; }
+    }
+}
--- Projeto_SalesWebMvc/Startup.cs
+++ Projeto_SalesWebMvc/Startup.cs
@@ -9,6 +9,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Projeto_SalesWebMvc.Data;
 
 namespace Projeto_SalesWebMvc
 {
@@ -33,6 +35,9 @@ namespace Projeto_SalesWebMvc
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<Projeto_SalesWebMvcContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("Projeto_SalesWebMvcContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

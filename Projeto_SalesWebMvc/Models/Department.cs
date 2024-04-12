680e277e020f69763eb4d5c15c3826be6481d3f9
Wesley Martins <wesley.mlopes@castgroup.com>
2024-04-11 22:48:07 -0300

    sistema de vendas

---
+++ Projeto_SalesWebMvc/Models/Department.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projeto_SalesWebMvc.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+
+    }
+}

51081bb7102d120f9fa1a71e9deb7e4a1a8a6dbd
Wesley Martins <wesley.mlopes@castgroup.com>
2024-04-14 23:12:10 -0300

    projeto finalizado com a busca em agrupamento

--- Projeto_SalesWebMvc/Models/SalesRecord.cs
+++ Projeto_SalesWebMvc/Models/SalesRecord.cs
@@ -1,6 +1,7 @@
 ﻿using Projeto_SalesWebMvc.Models.Enums;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,7 +10,11 @@ namespace Projeto_SalesWebMvc.Models
     public class SalesRecord
     {
         public int Id { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime Date { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double Amount { get; set; }
         public SaleStatus Status { get; set; }
         public Seller Seller { get; set; }
--- Projeto_SalesWebMvc/Services/SalesRecordService.cs
+++ Projeto_SalesWebMvc/Services/SalesRecordService.cs
@@ -32,5 +32,20 @@ namespace Projeto_SalesWebMvc.Services
             return await result.Include(x => x.Seller).Include(x => x.Seller.Department).OrderByDescending(x => x.Date).ToListAsync();
         }
 
+        public async Task<List<IGrouping<Department, SalesRecord>>> FindByDateGroupingAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from obj in _context.SalesRecord select obj;
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.Date >= minDate.Value);
+            }
+            if (maxDate.HasValue)
+            {
+                result = result.Where(x => x.Date <= maxDate.Value);
+            }
+
+            return await result.Include(x => x.Seller).Include(x => x.Seller.Department).OrderByDescending(x => x.Date).GroupBy(x => x.Seller.Department).ToListAsync();
+        }
+
     }
 }

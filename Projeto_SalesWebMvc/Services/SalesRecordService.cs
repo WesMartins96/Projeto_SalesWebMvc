eb6263882d3fe2fb539829365eb95710a49f100e
Wesley Martins <wesley.mlopes@castgroup.com>
2024-04-14 22:27:21 -0300

    implemtacao da busca por datas simples

---
+++ Projeto_SalesWebMvc/Services/SalesRecordService.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Projeto_SalesWebMvc.Data;
+using Projeto_SalesWebMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projeto_SalesWebMvc.Services
+{
+    public class SalesRecordService
+    {
+        private readonly Projeto_SalesWebMvcContext _context;
+
+        public SalesRecordService(Projeto_SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<SalesRecord>> FindByDateAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from obj in _context.SalesRecord select obj;
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.Date >= minDate.Value);
+            }
+            if (maxDate.HasValue)
+            {
+                result = result.Where(x => x.Date <= maxDate.Value);
+            }
+
+            return await result.Include(x => x.Seller).Include(x => x.Seller.Department).OrderByDescending(x => x.Date).ToListAsync();
+        }
+
+    }
+}
--- Projeto_SalesWebMvc/Startup.cs
+++ Projeto_SalesWebMvc/Startup.cs
@@ -45,6 +45,7 @@ namespace Projeto_SalesWebMvc
             services.AddScoped<SeedingService>();
             services.AddScoped<SellerService>();
             services.AddScoped<DepartmentService>();
+            services.AddScoped<SalesRecordService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
